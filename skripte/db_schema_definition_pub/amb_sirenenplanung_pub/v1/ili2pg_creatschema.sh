08766970e2592a3d7606eeab57ec108d46b24134
barpasen <peter.senn@bd.so.ch>
2020-11-12 10:48:50 +0100

    Name muss "_Publikation" enthalten

--- skripte/db_schema_definition_pub/amb_sirenenplanung_pub/v1/ili2pg_creatschema.sh
+++ skripte/db_schema_definition_pub/amb_sirenenplanung_pub/v1/ili2pg_creatschema.sh
@@ -1,4 +1,5 @@
-java -jar /usr/local/ili2pg-4.3.1/ili2pg.jar --dbschema amb_sirenenplanung_pub --models SO_AMB_Sirenenplanung_pub_20200831 \
+java -jar /usr/local/ili2pg-4.3.1/ili2pg.jar --modeldir '/sogis/daten_tools/skripte/db_schema_definition_pub/migration_sogis-db/amb_sirenenplanung_pub/v1;http://models.geo.admin.ch' \
+--dbschema amb_sirenenplanung_pub --models SO_AMB_Sirenenplanung_Publikation_20200831 \
 --defaultSrsCode 2056 --strokeArcs --createGeomIdx --createFk --createFkIdx --createEnumTabs --beautifyEnumDispName \
 --createMetaInfo --createUnique --createNumChecks --nameByTopic \
 --createscript amb_sirenenplanung_pub.sql
