a63e59c89bd36325df1e02f93aa617562016c569
barpasen <peter.senn@bd.so.ch>
2020-11-26 11:03:03 +0100

    Grundwasserisohypsen neu im Modell SO_AFU_Grundwassergeometrie_20201201

--- skripte/db_schema_definition_pub/afu_grundwasserisohypsen_pub/v1/ili2pg_creatschema.sh
+++
@@ -1,4 +0,0 @@
-java -jar /usr/local/ili2pg-4.3.1/ili2pg.jar --dbschema afu_grundwasserisohypsen_pub --models SO_AFU_Grundwasserisohypsen_Publikation_20201105 \
---defaultSrsCode 2056 --strokeArcs --createGeomIdx --createFk --createFkIdx --createEnumTabs --beautifyEnumDispName \
---createMetaInfo --createUnique --createNumChecks --nameByTopic \
---createscript afu_grundwasserisohypsen_pub.sql
