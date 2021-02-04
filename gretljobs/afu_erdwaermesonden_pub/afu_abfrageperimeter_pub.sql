f8196052bdc55289fdabffdeab580e72397cf990
barpasen <peter.senn@bd.so.ch>
2021-02-04 06:47:48 +0100

    falsche Bezeichnung

--- gretljobs/afu_erdwaermesonden_pub/afu_abfrageperimeter_pub.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    geometrie,
-    abfrage
-FROM
-    afu_erdwaermesonden_weitere.erdwarmsndn_wtere_abfrageperimeter
-;
--- gretljobs/afu_erdwaermesonden_pub/afu_private_quelle_pub.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    geometrie
-FROM
-    afu_erdwaermesonden_weitere.erdwarmsndn_wtere_private_quelle
-;
