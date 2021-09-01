9038ec06c632e675b5f7c62c364848324c5a9034
barpasen <peter.senn@bd.so.ch>
2021-09-01 11:03:59 +0200

    new model SO_AWJF_Silvaprotect_20210831

--- skripte/db_schema_definition_edit/awjf_silvaprotect/v1/ili2pg_createschema.sh
+++ skripte/db_schema_definition_edit/awjf_silvaprotect/v1/ili2pg_createschema.sh
@@ -1,4 +1,4 @@
-java -jar /usr/local/ili2pg-4.3.1/ili2pg.jar --modeldir 'http://models.geo.admin.ch;http://geo.so.ch/models;/sogis/daten_tools/skripte/db_schema_definition_edit/migration_sogis-db/awjf_silvaprotect/v1' --models SO_AWJF_Silvaprotect_20210831 \
+java -jar /usr/local/ili2pg-4.3.1/ili2pg.jar --modeldir 'http://models.geo.admin.ch;http://geo.so.ch/models' --models SO_AWJF_Silvaprotect_20210831 \
 --dbschema awjf_silvaprotect --defaultSrsCode 2056 --strokeArcs --createGeomIdx --createFk --createFkIdx --createEnumTabs --beautifyEnumDispName \
 --createMetaInfo --createUnique --createNumChecks --nameByTopic \
 --createscript awjf_silvaprotect.sql
