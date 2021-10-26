02b0e46a9b5094d76c227133c1898b3dfb3f46ca
barpasen <peter.senn@bd.so.ch>
2021-10-26 16:12:24 +0200

    grant select to public

--- skripte/db_schema_definition_edit/awjf_wildstrukuren/v1/postscript.sql
+++ skripte/db_schema_definition_edit/awjf_wildstrukuren/v1/postscript.sql
@@ -4,18 +4,16 @@ IS
     'Erfassungsmodell für Wildstrukturen. Fragen: mark.struch@vd.so.ch'
 ;
 
-CREATE ROLE awjf_wildstrukturen_read
-;
 CREATE ROLE awjf_wildstrukturen_write
 ;
 
-GRANT USAGE ON SCHEMA awjf_wildstrukturen TO public, gretl, awjf_wildstrukturen_read, awjf_wildstrukturen_write
+GRANT USAGE ON SCHEMA awjf_wildstrukturen TO public, gretl, awjf_wildstrukturen_write
 ;
---GRANT SELECT ON TABLE awjf_wildstrukturen.wildtierkorridor TO public
---;
+GRANT SELECT ON ALL TABLES IN SCHEMA awjf_wildstrukturen TO public
+;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA awjf_wildstrukturen TO gretl, awjf_wildstrukturen_write
 ;
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA awjf_wildstrukturen TO gretl, awjf_wildstrukturen_write
 ;
-GRANT awjf_wildstrukturen_write TO vkfaa, gdi_write
+GRANT awjf_wildstrukturen_write TO vkfaacot, vkfaascm, gdi_write
 ;
