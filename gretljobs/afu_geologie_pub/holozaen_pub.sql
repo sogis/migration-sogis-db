41b4480d76c08ebd1e56921f53a7f68d65579f64
barpasen <peter.senn@bd.so.ch>
2021-11-09 15:31:42 +0100

    [afu_geologie_pub] add dispname

--- gretljobs/afu_geologie_pub/holozaen_pub.sql
+++ gretljobs/afu_geologie_pub/holozaen_pub.sql
@@ -1,30 +1,30 @@
 SELECT 
     ST_MakeValid(ST_RemoveRepeatedPoints(ST_SnapToGrid(geometrie, 0.001))) AS geometrie,
-    system1,
-    system2,
-    serie1,
-    serie2,
-    formation1,
-    formation2,
-    schichtgliederung,
-    ausbildung_festgestein,
-    lithologie_festgestein,
-    sackung_festgestein,
-    ausbildung_lockergestein,
-    lithologie_lockergestein,
-    lithologie1,
-    lithologie2,
-    verkittungsgrad_lockergestein,
-    wasserdurchlaessigkeit,
-    grundwasser_art,
-    grundwasserfuehrung_maechtigkeit,
-    grundwassertyp,
-    reibungswinkel_lockergestein,
-    kohaesion_lockergestein,
-    reibungswinkel_festgestein,
-    kohaesion_festgestein,
-    maechtigkeit_material,
-    fehlendes_material_bis_naechster_layer
+    system1_code.dispname AS system1,
+    system2_code.dispname AS system2,
+    serie1_code.dispname AS serie1,
+    serie2_code.dispname AS serie2,
+    formation1_code.dispname AS formation1,
+    formation2_code.dispname AS formation2,
+    schichtgliederung_code.dispname AS schichtgliederung,
+    ausbildung_festgestein_code.dispname AS ausbildung_festgestein,
+    lithologie_festgestein_code.dispname AS lithologie_festgestein,
+    sackung_festgestein_code.dispname AS sackung_festgestein,
+    ausbildung_lockergestein_code.dispname AS ausbildung_lockergestein,
+    lithologie_lockergestein_code.dispname AS lithologie_lockergestein,
+    lithologie1_code.dispname AS lithologie1,
+    lithologie2_code.dispname AS lithologie2,
+    verkittungsgrad_lockergestein_code.dispname AS verkittungsgrad_lockergestein,
+    wasserdurchlaessigkeit_code.dispname AS wasserdurchlaessigkeit,
+    grundwasser_art_code.dispname AS grundwasser_art,
+    grundwasserfuehrung_maechtigkeit_code.dispname AS grundwasserfuehrung_maechtigkeit,
+    grundwassertyp_code.dispname AS grundwassertyp,
+    reibungswinkel_lockergestein_code.dispname AS reibungswinkel_lockergestein,
+    kohaesion_lockergestein_code.dispname AS kohaesion_lockergestein,
+    reibungswinkel_festgestein_code.dispname AS reibungswinkel_festgestein,
+    kohaesion_festgestein_code.dispname AS kohaesion_festgestein,
+    maechtigkeit_material_code.dispname AS maechtigkeit_material,
+    fehlendes_material_bis_naechster_layer_code.dispname AS fehlendes_material_bis_naechster_layer
 FROM 
     afu_geologie_v1.geologie_holozaen AS holozaen
     LEFT JOIN afu_geologie_v1.geologisches_system AS system1_code
@@ -47,8 +47,8 @@ FROM
     ON lithologie_festgestein = lithologie_festgestein_code.ilicode
     LEFT JOIN afu_geologie_v1.ausbildung_lockergestein AS ausbildung_lockergestein_code
     ON ausbildung_lockergestein = ausbildung_lockergestein_code.ilicode
-    LEFT JOIN afu_geologie_v1.lithologie AS lithologie_code
-    ON lithologie_lockergestein = lithologie_code.ilicode
+    LEFT JOIN afu_geologie_v1.lithologie AS lithologie_lockergestein_code
+    ON lithologie_lockergestein = lithologie_lockergestein_code.ilicode
     LEFT JOIN afu_geologie_v1.geologie_geologische_schicht_sackung_festgestein AS sackung_festgestein_code
     ON sackung_festgestein = sackung_festgestein_code.ilicode
     LEFT JOIN afu_geologie_v1.lithologie AS lithologie1_code
