6e6856085fc4fc0305c768b1bde26481dde60d04
barpasen <peter.senn@bd.so.ch>
2021-11-10 11:54:30 +0100

    [afu_baugrundklassen] Add versions-suffix

--- gretljobs/afu_baugrundklassen/make_valid_geometry.sql
+++ gretljobs/afu_baugrundklassen/make_valid_geometry.sql
@@ -1,3 +1,3 @@
-UPDATE afu_baugrundklassen.baugrundklassen_baugrundklasse
+UPDATE afu_baugrundklassen_v1.baugrundklassen_baugrundklasse
     SET geometrie = ST_MakeValid(ST_RemoveRepeatedPoints(ST_SnapToGrid(geometrie, 0.001)))
 ;
